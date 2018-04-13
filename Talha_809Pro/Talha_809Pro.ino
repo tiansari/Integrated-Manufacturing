a035547078613c543bc96be7c7e5de3766ebb86d
T. Ansari <talhairfan17@gmail.com>
2018-04-12 20:30:14 -0400

    Add files via upload

--- Talha_809Pro/Talha_809Pro.ino
+++ Talha_809Pro/Talha_809Pro.ino
@@ -6,8 +6,8 @@
 EVShield evshield (0x34, 0x36);
 EVs_NXTTouch touch1, touch2;
 
-int EN_Mtr2_1,EN_Mtr1_1; // Position of Motors at Location A 
-int EN_Mtr2_2,EN_Mtr1_2; // Position of Motors at Location B
+float EN_Mtr2_1,EN_Mtr1_1; // Position of Motors at Location A 
+float EN_Mtr2_2,EN_Mtr1_2; // Position of Motors at Location B
 
 // This function is to setup motors and Sensors
 void setup() {  
@@ -44,7 +44,8 @@ void GetCoordinatesA() {
      Serial.println("Calculating....");
      delay(2000);
      Serial.println("Rotation for Motor1 and 2 is");
-     Serial.println(EN_Mtr1_1, EN_Mtr2_1);
+     Serial.println(EN_Mtr1_1);
+     Serial.println(EN_Mtr2_1);
      Serial.println("Location A acquired!");
 //     if (EN_Mtr2_1<0)
 //     evshield.bank_b.motorRunDegrees(SH_Motor_2, SH_Direction_Forward,SH_Speed_Slow,EN_Mtr2_1, SH_Completion_Wait_For, SH_Next_Action_Float);
@@ -74,7 +75,8 @@ void GetCoordinatesB() {
      Serial.println("Calculating....");
      delay(2000);
      Serial.println("Rotation for Motor1 and 2 is");
-     Serial.println(EN_Mtr1_2, EN_Mtr2_2);
+     Serial.println(EN_Mtr1_2);
+     Serial.println(EN_Mtr2_2);
      Serial.println("Location B acquired!");
 
      //evshield.bank_a.motorRunDegrees(SH_Motor_2, SH_Direction_Reverse,SH_Speed_Slow, 0, SH_Completion_Wait_For, SH_Next_Action_Float);
@@ -82,44 +84,46 @@ void GetCoordinatesB() {
 
 //This function is called to execute taught sequence
 void execute (){
-      Serial.println("Executing...");
+     Serial.println("Executing...");
 
-      //Open Claw
-      evshield.bank_b.motorRunDegrees(SH_Motor_1, SH_Direction_Forward,SH_Speed_Slow,50, SH_Completion_Wait_For, SH_Next_Action_BrakeHold);
-      delay(2000);
-     if (EN_Mtr2_2<0)
-     evshield.bank_b.motorRunDegrees(SH_Motor_2, SH_Direction_Forward,SH_Speed_Slow,EN_Mtr2_2, SH_Completion_Wait_For, SH_Next_Action_Float);
-     else{
-      evshield.bank_b.motorRunDegrees(SH_Motor_2, SH_Direction_Reverse,SH_Speed_Slow,EN_Mtr2_2, SH_Completion_Wait_For, SH_Next_Action_Float);
-     }        
+     //Open Claw
+     evshield.bank_b.motorRunDegrees(SH_Motor_1, SH_Direction_Reverse,SH_Speed_Slow,90, SH_Completion_Wait_For, SH_Next_Action_BrakeHold);
+     Serial.println("Claw opened"); 
+        
      if (EN_Mtr1_2<0)
      evshield.bank_a.motorRunDegrees(SH_Motor_1, SH_Direction_Forward,SH_Speed_Slow,EN_Mtr1_2, SH_Completion_Wait_For, SH_Next_Action_Float);
      else{
       evshield.bank_a.motorRunDegrees(SH_Motor_1, SH_Direction_Reverse,SH_Speed_Slow,EN_Mtr1_2, SH_Completion_Wait_For, SH_Next_Action_Float);
      }
+     if (EN_Mtr2_2<0)
+     evshield.bank_b.motorRunDegrees(SH_Motor_2, SH_Direction_Forward,SH_Speed_Slow,EN_Mtr2_2, SH_Completion_Wait_For, SH_Next_Action_Float);
+     else{
+      evshield.bank_b.motorRunDegrees(SH_Motor_2, SH_Direction_Reverse,SH_Speed_Slow,EN_Mtr2_2, SH_Completion_Wait_For, SH_Next_Action_Float);
+     }     
      //evshield.bank_a.motorReset();
      delay(2000);
      //Close Claw
-     evshield.bank_b.motorRunDegrees(SH_Motor_1, SH_Direction_Reverse,SH_Speed_Slow,50, SH_Completion_Wait_For, SH_Next_Action_BrakeHold);
+     evshield.bank_b.motorRunDegrees(SH_Motor_1, SH_Direction_Forward,SH_Speed_Slow,90, SH_Completion_Wait_For, SH_Next_Action_BrakeHold);
      Serial.println("Object Picked...");
      delay(2000);
       
      Serial.println("Dropping now...");
-     if (EN_Mtr2_2<0)
-     evshield.bank_b.motorRunDegrees(SH_Motor_2, SH_Direction_Reverse,SH_Speed_Slow,EN_Mtr2_2, SH_Completion_Wait_For, SH_Next_Action_Float);
-     else{
-      evshield.bank_b.motorRunDegrees(SH_Motor_2, SH_Direction_Forward,SH_Speed_Slow,EN_Mtr2_2, SH_Completion_Wait_For, SH_Next_Action_Float);
-     }        
+        
      if (EN_Mtr1_2<0)
      evshield.bank_a.motorRunDegrees(SH_Motor_1, SH_Direction_Reverse,SH_Speed_Slow,EN_Mtr1_2, SH_Completion_Wait_For, SH_Next_Action_Float);
      else{
       evshield.bank_a.motorRunDegrees(SH_Motor_1, SH_Direction_Forward,SH_Speed_Slow,EN_Mtr1_2, SH_Completion_Wait_For, SH_Next_Action_Float);
      }
-     //Open Claw
-     evshield.bank_b.motorRunDegrees(SH_Motor_1, SH_Direction_Forward,SH_Speed_Slow,50, SH_Completion_Wait_For, SH_Next_Action_BrakeHold);
-     Serial.println("Object Dropped...");
-     delay(2000);
+     if (EN_Mtr2_2<0)
+     evshield.bank_b.motorRunDegrees(SH_Motor_2, SH_Direction_Reverse,SH_Speed_Slow,EN_Mtr2_2, SH_Completion_Wait_For, SH_Next_Action_Float);
+     else{
+      evshield.bank_b.motorRunDegrees(SH_Motor_2, SH_Direction_Forward,SH_Speed_Slow,EN_Mtr2_2, SH_Completion_Wait_For, SH_Next_Action_Float);
+     }
      
+     //Open Claw
+     evshield.bank_b.motorRunDegrees(SH_Motor_1, SH_Direction_Reverse,SH_Speed_Slow,90, SH_Completion_Wait_For, SH_Next_Action_BrakeHold);
+     Serial.println("Object dropped"); 
+     delay(2000);
      //Close Claw
      //evshield.bank_b.motorRunDegrees(SH_Motor_1, SH_Direction_Reverse,SH_Speed_Slow,90, SH_Completion_Wait_For, SH_Next_Action_Float);
      Serial.println("Execution Complete!");     
